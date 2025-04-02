b47626ae4bd1db4b50eae163a754c6f771a4d3de
Mateusz Dobrzyński <mateusz.dobrzynski64@gmail.com>
2025-04-02 16:25:06 +0200

    feat: prevent voting multiple times

--- 02 django/mysite/polls/urls.py
+++ 02 django/mysite/polls/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path('answers', views.answers, name='answers'),
-    path('poll', views.poll, name='poll'),
-    path('save', views.save, name='save'),
+    path("answers", views.answers, name="answers"),
+    path("poll", views.poll, name="poll"),
+    path("poll/<str:token>/", views.poll_with_token, name="poll_with_token"),
+    path("save", views.save, name="save"),
 ]
--- 02 django/mysite/polls/views.py
+++ 02 django/mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Question
 from .models import Choice
+from .models import Token
 from django.template import loader
 
 # Create your views here.
@@ -10,27 +11,50 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
+
 def answers(request):
     latest_question_list = Question.objects.all()
-    template = loader.get_template('polls/answers.html')
+    template = loader.get_template("polls/answers.html")
     context = {
-    'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))   
-
-def poll(request):
-    latest_question_list = Question.objects.all()
-    template = loader.get_template('polls/poll.html')
-    context = {
-    'latest_question_list': latest_question_list,
+        "latest_question_list": latest_question_list,
     }
     return HttpResponse(template.render(context, request))
 
+
+def poll(request):
+    latest_question_list = Question.objects.all()
+    template = loader.get_template("polls/poll.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def poll_with_token(request, token):
+    latest_question_list = Question.objects.all()
+    template = loader.get_template("polls/poll.html")
+    context = {
+        "latest_question_list": latest_question_list,
+        "token": token,
+    }
+    print(f"token: {token}")
+    return HttpResponse(template.render(context, request))
+
+
 def save(request):
-    if request.method == 'GET':
-        for item in request.GET:
-            if item != "csrfmiddlewaretoken":
-                selected_choice = Choice.objects.get(choice_text=request.GET[item])
-                selected_choice.votes += 1
-                selected_choice.save()
-    return HttpResponse("Answers saved!")
+    token = request.GET.get("token", None)
+    if token:
+        try:
+            correspondingToken = Token.objects.get(token=token)
+            return HttpResponse("You are not allowed to vote again")
+        except Exception as e:
+            print("bajojajo", e)
+            for item in request.GET:
+                if item != "csrfmiddlewaretoken" and item != "token":
+                    selected_choice = Choice.objects.get(choice_text=request.GET[item])
+                    selected_choice.votes += 1
+                    selected_choice.save()
+            used_token = Token()
+            used_token.token = token
+            used_token.save()
+            return HttpResponse("Answers saved!")
